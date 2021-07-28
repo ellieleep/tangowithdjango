940c3ab1a3d6fcc64df43840182774b9d195cddd
ellieleep <32715340+ellieleep@users.noreply.github.com>
2021-07-27 21:28:22 -0400

    updated for exercises

--- Chapter 3/tango_with_django_project/rango/views.py
+++ Chapter 3/tango_with_django_project/rango/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse('<a href="http://127.0.0.1:8000/rango/about/">About</a>')
+
+
+def about(request):
+    return HttpResponse('<a href="http://127.0.0.1:8000/rango/">Index</a>')
