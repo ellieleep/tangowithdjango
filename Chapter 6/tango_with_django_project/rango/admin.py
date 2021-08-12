25026877209bcd7ce1a5e245e05aa0ad5980ebdd
ellieleep <ellie.leepierce@gmail.com>
2021-08-12 13:22:39 -0400

    initial chapter 6 commit

---
+++ Chapter 6/tango_with_django_project/rango/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Page
+from .models import Category
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'url']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'views', 'likes']
+
+
+admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CategoryAdmin)
